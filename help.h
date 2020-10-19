bb7a24a01cae27d8c317f3c0d236d000001e5425
mkhomo <mkhomo@C4-E9-84-02-DA-ED>
2020-10-19 22:49:44 +0200

    1st Git Commit from CentOS 8

---
+++ help.h
@@ -0,0 +1,39 @@
+void default_prnt(void);
+
+void lang_prnt(void);
+
+void actions_prnt(void);
+
+void radx_prnt(void);
+
+void tag_prnt(void);
+
+void trace_prnt(void);
+
+void prec_prnt(int *option);
+
+void fctr_prnt(void);
+
+void algr_prnt(int *option);
+
+void uoprs_prnt(void);
+
+void boprs_prnt(void);
+
+void nopt_prnt(int option);
+
+void iusage_prnt(void);
+
+void busage_prnt(void);
+
+void fnct_prnt(void);
+
+void help_prnt(void);
+
+void clx_prnt(void);
+
+void intro_prnt(void);
+
+void gbye_prnt(void);
+
+void cpyrgt_prnt(void);
