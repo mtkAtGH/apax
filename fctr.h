8c82c1ccc9a828be5b5314b9d576284538caf9ec
mkhomo <mkhomo@C4-E9-84-02-DA-ED>
2020-10-19 22:48:24 +0200

    1st Git Commit from CentOS 8

---
+++ fctr.h
@@ -0,0 +1,14 @@
+int *fctadd(int *str1,int *str2);
+int *fctsubt(int *first, int *second);
+int *fctminus(int *x,int *y);
+int *fctmult(int *str1,int *str2);
+int *fctdivd(int *str1,int *str2);
+int *fctimul(int *str1,int *str2);
+int *fctidiv(int *str1,int *str2);
+int *fctmuli(int *str,int num) ;
+int *fctdivi(int *str,int num);
+int *intofs(int value);
+int *dstofs(int *dstr);
+int *dectof(int *decml);
+int *fintod(int *fctin);
+int *fcttod(int *factrl, int sigdgt);
