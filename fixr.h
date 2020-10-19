3466708bd548aa0d3b006fbe051550d1bb009a2e
mkhomo <mkhomo@C4-E9-84-02-DA-ED>
2020-10-19 22:48:51 +0200

    1st Git Commit from CentOS 8

---
+++ fixr.h
@@ -0,0 +1,91 @@
+int *add(int *str1,int *str2);
+
+int *minus(int *x, int *y);
+
+int *subt(int *first, int *second);
+
+int *mult(int *one, int *other);
+
+int *submul(int pos, int *one, int *other);
+
+int *divd(int *str1,int *str2,int sigdgt);
+
+int *remd(int *str1,int *str2,int sigdgt);
+
+int *divrem(int **str0,int *str1,int *str2,int sigdgt);
+
+int *muli(int *str,int num);
+
+int *divi(int *str,int num,int sigdgt);
+
+int *idivmod(int *rem,int *str,int num);
+
+int *idiv(int *str1,int *str2);
+
+int *modl(int *str1,int *str2);
+
+int *divmod(int **rem,int *str1,int *str2);
+
+int *fctrial(int *decin);
+
+int *ptadrs(int *num);
+
+int ordhsd(int *num);
+
+int greater(int *str1,int *str2);
+
+int equal(int *str1,int *str2);
+
+int zero(int *num);
+
+int *shift(int *num,int count);
+
+int *rgtshift(int *num, int count);
+
+int *lftshift(int *num, int count);
+
+int sumdgt(int *str);
+
+int intlen(int *flt);
+
+int frclen(int *flt);
+
+int chrpos(int *str, int chr);
+
+int sumlen(int *str1,int *str2);
+
+int *trim(int *num);
+
+int *lftrim(int *num);
+
+int stor10i( int *str);
+
+int *rgtrim(int *num);
+
+int *dstors(int *dstr);
+
+int strtoi(int *str);
+
+int *inttos(int value);
+
+char *intocs(int value);
+
+int *dbltos(double value);
+
+char *dbtocs(double value);
+
+int *flttos(void);
+
+char *fltocs(void);
+
+int *fraction(int *str);
+
+int *integer(int *str);
+
+int min(int int1, int int2);
+
+int max(int int1, int int2);
+
+int iabs(int num);
+
+int *hisgdt(int *num);
